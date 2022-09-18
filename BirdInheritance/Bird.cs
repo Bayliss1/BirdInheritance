82ab1270af2d2ba09f2555dd6d31bf6b99939f1f
Owen <owen.bayliss@sky.com>
2022-09-18 11:42:50 +0100

    Bird class done

---
+++ BirdInheritance/Bird.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdInheritance
+{
+    class Bird
+    {
+        public static Random Randomizer = new Random();
+        public virtual Egg[] LayEggs(int numberOfEggs)
+        {
+            Console.Error.WriteLine("Bird.LayEggs should never get called");
+            return new Egg[0];
+        }
+    }
+}
