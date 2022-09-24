a28ba6efbdf601ffad3bc825f1f5720e4593a67f
Owen <owen.bayliss@sky.com>
2022-09-24 17:41:25 +0100

    Added BrokenEgg class and IF statement to Pigeon class

---
+++ BirdInheritance/BrokenEgg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BirdInheritance
+{
+    class BrokenEgg : Egg
+    {
+        public BrokenEgg(double size, string color): base(size, color)
+        {
+            Console.WriteLine("A bird laid a broken egg");
+        }
+    }
+}
--- BirdInheritance/Pigeon.cs
+++ BirdInheritance/Pigeon.cs
@@ -13,7 +13,8 @@ namespace BirdInheritance
             Egg[] eggs = new Egg[numberOfEggs];
             for (int i = 0; i < numberOfEggs; i++)
             {
-                eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white");
+                if (Bird.Randomizer.Next(4) == 0) { eggs[i] = new BrokenEgg(Bird.Randomizer.NextDouble() * 2 + 1, "white"); }
+                else { eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white"); }
             }
             return eggs;
         }
