4a9dd8375be95114c08bf93f4a9e2fb1d9264419
Owen <owen.bayliss@sky.com>
2022-10-01 13:38:05 +0100

    Fixed BrokenEgg constructor / Pigeon class

--- BirdInheritance/Pigeon.cs
+++ BirdInheritance/Pigeon.cs
@@ -13,7 +13,7 @@ namespace BirdInheritance
             Egg[] eggs = new Egg[numberOfEggs];
             for (int i = 0; i < numberOfEggs; i++)
             {
-                if (Bird.Randomizer.Next(4) == 0) { eggs[i] = new BrokenEgg(Bird.Randomizer.NextDouble() * 2 + 1, "white"); }
+                if (Bird.Randomizer.Next(4) == 0) { eggs[i] = new BrokenEgg("white"); }
                 else { eggs[i] = new Egg(Bird.Randomizer.NextDouble() * 2 + 1, "white"); }
             }
             return eggs;
